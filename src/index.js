d950c65b5106444395ebc9f9e8cc4136d48bff03
BrknHnds <bsbd42@yandex.ru>
2023-10-22 20:44:20 +0500

    feat: checkpiont for 22.10.2023 2023-10-22 Вс 20:44:19

--- src/index.js
+++ src/index.js
@@ -22,15 +22,172 @@
 // }
 
 
+
+// second 17 / 20
+// module.exports = function check(str, bracketsConfig) {
+//   bracketsConfig.forEach( (e, i) => {
+//     if (e[0] === e[1]) {
+//       e[0] = `${i + 3}`;
+//       e[1] = `${i + 4}`;
+//     }
+//   });
+
+//   let strArray = str.split('');
+//   if (strArray.length % 2 !== 0) return false;
+//   let openSymbolsArray = bracketsConfig.map(e => e[0]);
+//   let closeSymbolsArray = bracketsConfig.map(e => e[1]);
+//   let compareArray = [];
+//   let openIndex = null;
+//   let closeIndex = null;
+
+//   for (let i = 0; i < strArray.length; i++) {
+//     openIndex = openSymbolsArray.indexOf(strArray[i]);
+//     if (openIndex !== -1) {
+//       compareArray.push(openIndex);
+//     }
+//     closeIndex = closeSymbolsArray.indexOf(strArray[i]);
+//     if (closeIndex !== -1) {
+//       openIndex = compareArray.pop();
+//       if (openIndex !== closeIndex) return false;
+//     }
+//   }
+//   if (compareArray.length === 0) {
+//     return true;
+//   } else return false;
+// }
+
+// second
+// function check(str, bracketsConfig) {
+//   let strArray = str.split('');
+//   let openSymbolsArray = bracketsConfig.map(e => e[0]);
+//   let closeSymbolsArray = bracketsConfig.map(e => e[1]);
+//   let compareArray = [];
+//   let openIndex = null;
+//   let closeIndex = null;
+
+//   for (let i = 0; i < strArray.length; i++) {
+//     openIndex = openSymbolsArray.indexOf(strArray[i]);
+//     if (openIndex !== -1) {
+//       compareArray.push(openIndex);
+//     }
+//     closeIndex = closeSymbolsArray.indexOf(strArray[i]);
+//     if (closeIndex !== -1) {
+//       openIndex = compareArray.pop();
+//       if (openIndex !== closeIndex) return false;
+//     }
+//   }
+//   if (compareArray.length === 0) return true;
+// }
+// console.log(check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]));
+
+// temp
+// var test = function(str) {
+//   var chars = str.split(''),
+//       stack = [],
+//       open = ['{', '(', '['],
+//       close = ['}', ')', ']'],
+//       closeIndex,
+//       openIndex;
+//   // Проходимся по строке, проверяя каждый ее символ (п.4).
+//   for (var i = 0, len = chars.length; i < len; i++) {
+//      openIndex = open.indexOf(chars[i]);
+//      console.log(openIndex);
+//      if (openIndex !== -1) {
+//          // Нашли открывающую скобку. Помещаем ее в стек (п.2).
+//          stack.push(openIndex);
+//          continue;
+//      }
+//      closeIndex = close.indexOf(chars[i]);
+//      if (closeIndex !== -1) {
+//          // Нашли закрывающую скобку. Проверяем ее соответствие открывающей (п.3).
+//          openIndex = stack.pop();
+//          if (closeIndex !== openIndex) {
+//              return false;
+//          }
+//      }
+//   }
+//   // Проверяем дисбаланс открытых/закрытых скобок (п.5).
+//   if (stack.length !== 0) {
+//       return false;
+//   }
+//   return true;
+// }
+
+// console.log(test('text with([brackets])')); // true
+// console.log(test('text with ([wrong brackets)]')); // false
+// console.log(test('text with [wrong brackets')); // false
+
+
+
+//third
+// module.exports = function check(str, bracketsConfig) {
+
+//   // заменяем повторяющийся разделитель на уникальный сначала в str, потом в bracketsConfig
+//   let strArray = str.split('');
+//   if (strArray.length % 2 !== 0) return false;
+//   // console.log(strArray);
+//   bracketsConfig.forEach((e, i) => {
+//     if (e[0] === e[1]) {
+//       console.log(strArray[strArray.indexOf(e[0])]);
+//       console.log(strArray[strArray.indexOf(e[0], strArray.indexOf(e[0]) + 1)]);
+//       strArray[strArray.indexOf(e[0])] = `${i + 1}`;
+//       // strArray[strArray.indexOf(e[0], strArray.indexOf(e[0]) + 1)] = `${i + 2}`;
+//       strArray[strArray.indexOf(e[0])] = `${i + 2}`;
+//       e[0] = `${i + 1}`;
+//       e[1] = `${i + 2}`;
+//     }
+//   });
+//   console.log(strArray);
+
+
+
+//   let openSymbolsArray = bracketsConfig.map(e => e[0]);
+//   let closeSymbolsArray = bracketsConfig.map(e => e[1]);
+//   let compareArray = [];
+//   let openIndex = null;
+//   let closeIndex = null;
+
+//   for (let i = 0; i < strArray.length; i++) {
+//     openIndex = openSymbolsArray.indexOf(strArray[i]);
+//     if (openIndex !== -1) {
+//       compareArray.push(openIndex);
+//     }
+//     closeIndex = closeSymbolsArray.indexOf(strArray[i]);
+//     if (closeIndex !== -1) {
+//       openIndex = compareArray.pop();
+//       if (openIndex !== closeIndex) return false;
+//     }
+//   }
+//   if (compareArray.length === 0) {
+//     return true;
+//   } else return false;
+// }
+
+
+// third
 function check(str, bracketsConfig) {
+
+  // заменяем повторяющийся разделитель на уникальный сначала в str, потом в bracketsConfig
   let strArray = str.split('');
-  // console.log(`strArray is ${strArray}`);
-  if (strArray.length % 2 !== 0) return false
+  if (strArray.length % 2 !== 0) return false;
+  // console.log(strArray);
+  bracketsConfig.forEach((e, i) => {
+    if (e[0] === e[1]) {
+      console.log(strArray[strArray.indexOf(e[0])]);
+      console.log(strArray[strArray.indexOf(e[0], strArray.indexOf(e[0]) + 1)]);
+      strArray[strArray.indexOf(e[0])] = `${i + 1}`;
+      // strArray[strArray.indexOf(e[0], strArray.indexOf(e[0]) + 1)] = `${i + 2}`;
+      strArray[strArray.indexOf(e[0])] = `${i + 2}`;
+      e[0] = `${i + 1}`;
+      e[1] = `${i + 2}`;
+    }
+  });
+  console.log(strArray);
+
+
 
   let openSymbolsArray = bracketsConfig.map(e => e[0]);
-  // console.log(`openSymbolsArray is ${openSymbolsArray}`);
   let closeSymbolsArray = bracketsConfig.map(e => e[1]);
-  // console.log(`closeSymbolsArray is ${closeSymbolsArray}`);
   let compareArray = [];
   let openIndex = null;
   let closeIndex = null;
@@ -42,9 +199,7 @@ function check(str, bracketsConfig) {
     }
     closeIndex = closeSymbolsArray.indexOf(strArray[i]);
     if (closeIndex !== -1) {
-      // console.log(`oi & ci before ${openIndex}, ${closeIndex}`)
       openIndex = compareArray.pop();
-      // console.log(`oi & ci after ${openIndex}, ${closeIndex}`)
       if (openIndex !== closeIndex) return false;
     }
   }
